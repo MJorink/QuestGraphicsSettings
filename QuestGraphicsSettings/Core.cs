b722430df22021da71991dc4c65825b9aaed0572
Max Jorink <maxjorink@gmail.com>
2026-01-12 23:14:49 +0100

    Removed useless fog logging

--- QuestGraphicsSettings/Core.cs
+++ QuestGraphicsSettings/Core.cs
@@ -4,7 +4,6 @@ using BoneLib.BoneMenu;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.SceneManagement;
 
 [assembly: MelonInfo(typeof(QuestGraphicsSettings.Core), "QuestGraphicsSettings", "1.1.2", "jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
@@ -24,7 +23,6 @@ namespace QuestGraphicsSettings {
 
         private Camera playerCamera;
         private GameObject fogObject;
-        private bool warnedMissingVolumetrics;
         
         public override void OnInitializeMelon() {
             MelonPrefs();
@@ -71,8 +69,6 @@ namespace QuestGraphicsSettings {
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
             base.OnSceneWasLoaded(buildIndex, sceneName);
-            fogObject = null;
-            warnedMissingVolumetrics = false;
             ApplySettings();
         }
 
@@ -100,16 +96,7 @@ namespace QuestGraphicsSettings {
             
             if (fogObject != null)
             {
-                warnedMissingVolumetrics = false;
                 fogObject.SetActive(FogEntry.Value);
-                return;
-            }
-
-            if (!warnedMissingVolumetrics)
-            {
-                warnedMissingVolumetrics = true;
-                string activeSceneName = SceneManager.GetActiveScene().name;
-                MelonLogger.Warning($"QuestGraphicsSettings: Fog object 'Volumetrics' was not found (scene='{activeSceneName}'). Fog toggle will have no effect for this scene.");
             }
         }
 
