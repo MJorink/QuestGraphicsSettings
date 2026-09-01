34d55e81f0599846d0879bfbb8516e175c7dff8c
Jorink <maxjorink@gmail.com>
2026-09-01 22:57:00 +0200

    Fix AssemblyInfo colors

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using questgraphicssettings;
 
 [assembly: MelonInfo(typeof(QuestGraphicsSettings), "QuestGraphicsSettings", QuestGraphicsSettings.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonColor(255,255,0,1)]
-[assembly: MelonAuthorColor(255,0,0,1)]
+[assembly: MelonColor(255,255,255,0)]
+[assembly: MelonAuthorColor(255,255,0,0)]
